6dea1f9f160e23c09f993e72ad49a6d7a16e91ec
Aratz M. Lasa <aratzml@opendeusto.es>
2019-02-18 18:25:52 +0100

    Deleted unused files

--- webGraph/_webpage.py
+++
@@ -1,8 +0,0 @@
-import attr
-
-
-@attr.s(cmp=False, hash=False, repr=False)
-class WebPage():
-    url = attr.ib(default=None)
-    html = attr.ib(default=None)
-    related_urls = attr.ib(default=attr.Factory(list))
