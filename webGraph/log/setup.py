d52e2e6ce358914f34758b65b5afd7e78ab27b7e
Aratz M. Lasa <aratzml@opendeusto.es>
2019-02-18 19:38:21 +0100

    Added relative logging yaml

--- webGraph/log/setup.py
+++ webGraph/log/setup.py
@@ -3,11 +3,13 @@ import logging.config
 import yaml
 
 
-def setup_logging(path="webGraph/data/log/config.yaml"):
+def setup_logging(path=None):
+    if not path:
+        path = os.path.join(os.path.dirname(__file__), "../data/log/config.yaml")
+
     if os.path.exists(path):
         with open(path, 'rt') as f:
             config = yaml.safe_load(f.read())
         logging.config.dictConfig(config)
     else:
-        absolute_path = os.path.realpath(path)
-        raise FileNotFoundError("Logging configuration file not found at {}".format(absolute_path ))
+        raise FileNotFoundError("Logging configuration file not found at {}".format(path))
